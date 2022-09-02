12d4afed811f63e87a61439dedfdbb0bae764a41
Alan Edward da Silva Oliveira <57070322+AlanEdward19@users.noreply.github.com>
2022-09-02 09:19:54 -0300

    Updates

---
+++ precache-manifest.db5fd3b3621a8324d720304faf340acb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbd49f8ee5777a9ecc67",
+    "url": "/RoboFriends/static/css/main.079931bc.chunk.css"
+  },
+  {
+    "revision": "bbd49f8ee5777a9ecc67",
+    "url": "/RoboFriends/static/js/main.bbd49f8e.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/RoboFriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "05df9e17041bc58c418a",
+    "url": "/RoboFriends/static/css/2.36e9a38d.chunk.css"
+  },
+  {
+    "revision": "05df9e17041bc58c418a",
+    "url": "/RoboFriends/static/js/2.05df9e17.chunk.js"
+  },
+  {
+    "revision": "4cb7496509aa3077e2b1",
+    "url": "/RoboFriends/static/js/runtime~main.4cb74965.js"
+  },
+  {
+    "revision": "26a6b5ab6f28bfc5e5aa006429964ed1",
+    "url": "/RoboFriends/index.html"
+  }
+];
